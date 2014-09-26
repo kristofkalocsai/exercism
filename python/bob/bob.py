75b2fd2c3bcbf5dd2d1d8be17b0436c05b7d5b78
kristofkalocsai <kristofkalocsai@gmail.com>
2014-09-26 11:27:52 +0200

    iteration 2
    without redundance

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,5 +1,5 @@
 def hey(sentence):
-    if sentence.startswith('') and sentence.strip() == '':
+    if sentence.strip() == '':
         return 'Fine. Be that way!'
     if sentence.isupper():
         return 'Whoa, chill out!'
